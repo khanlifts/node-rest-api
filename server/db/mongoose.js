eca9269ee789d597fecb375bd5d142c87b2698a9
Cyril Khan <cyrilkhan8@gmail.com>
2018-02-18 14:26:21 +0100

    Setup separate and developtment envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,7 @@
 var mongoose = require('mongoose');
-// if connection to heroku with process.env.MONGODB_URI
-// fails then connect to local db 
-var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(url);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose
