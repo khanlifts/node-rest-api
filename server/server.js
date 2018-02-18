54531ecfb7542e865b7a27bdda9477c4cd230150
Cyril Khan <cyrilkhan8@gmail.com>
2018-02-18 09:55:27 +0100

    add patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-var {ObjectID} = require('mongodb');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -22,7 +23,7 @@ app.post('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
-}); 
+});
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
@@ -58,6 +59,37 @@ app.delete('/todos/:id', (req, res) => {
     }
     res.status(200).send({todo});
   }).catch((e) => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  // pick creates object of chosen properties e.g. text, completed
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  // if body.completed is a boolean and is set to true --> completed: true
+  if (_.isBoolean(body.completed) && body.completed) {
+    // set new property completedAt with a timestamp of completion time
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    // set property to null because todo isn't completed
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(port, () => {
